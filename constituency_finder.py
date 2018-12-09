304a6dc8cabe654f04af9803663965acabaa0995
Tonmoy Borah <mailtonmoy2@gmail.com>
2018-12-09 21:28:33 +0530

    Update constituency_finder.py

--- constituency_finder.py
+++ constituency_finder.py
@@ -2,7 +2,7 @@ from geopandas import GeoSeries, GeoDataFrame
 from shapely.geometry import Polygon, Point, LineString
 
 
-assembly_data = GeoDataFrame.from_file('/home/nikhil/ML/maps/assembly-constituencies/India_AC.shp')
+assembly_data = GeoDataFrame.from_file('./maps/assembly-constituencies/India_AC.shp')
 
 # search in all data
 test_point = Point(77.083493, 28.470082)
